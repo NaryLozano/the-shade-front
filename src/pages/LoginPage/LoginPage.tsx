61478dc0bcdb8f9de3437b32c79268f40c062793
nary <narylozano@gmail.com>
2023-05-29 21:54:05 +0200

    Add login page with styles

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,14 @@
+import LoginPageStyled from "./LoginPageStyled";
+
+const LoginPage = (): React.ReactElement => {
+  return (
+    <LoginPageStyled>
+      <img
+        src="./media/Logotype.svg"
+        alt="The Shade of it all with drawing of Sasha Velour"
+      />
+    </LoginPageStyled>
+  );
+};
+
+export default LoginPage;
