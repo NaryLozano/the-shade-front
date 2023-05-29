916afc48ea8b516b5a5dccc420be4bb9a0424ca1
nary <narylozano@gmail.com>
2023-05-29 18:12:10 +0200

    Add appRouter router  and Routerprovider at main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fontsource/roboto-mono";
-import App from "./components/App/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { RouterProvider } from "react-router-dom";
+import appRouter from "./routers/appRouter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={appRouter} />
     </Provider>
   </React.StrictMode>
 );
---
+++ src/routers/appRouter.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "../components/App/App";
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+  },
+];
+
+const appRouter = createBrowserRouter(routes);
+
+export default appRouter;
