722e524209e7a1288b97f773588fc2441a1caa45
nary <narylozano@gmail.com>
2023-05-29 20:48:52 +0200

    Add Header component with styles and add it to Layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import Navigation from "../Navigation/Navigation";
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): React.ReactElement => {
+  return (
+    <HeaderStyled>
+      <img
+        className="logotype"
+        src="./media/Logotype.svg"
+        alt="The Shade of it all with drawing of Sasha Velour"
+      />
+      <Navigation></Navigation>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
