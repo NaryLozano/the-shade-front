65c65f2525ba3b1e7bc5ee27fbc39d2880f99a7f
nary <narylozano@gmail.com>
2023-05-29 21:54:55 +0200

    Add login page to router and update layout children

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,10 @@
-import Header from "../Header/Header";
+import { Outlet } from "react-router-dom";
 import ContainertStyled from "../shared/ContainerStyled";
 
 const Layout = (): React.ReactElement => {
   return (
     <ContainertStyled>
-      <Header></Header>
+      <Outlet />
     </ContainertStyled>
   );
 };
