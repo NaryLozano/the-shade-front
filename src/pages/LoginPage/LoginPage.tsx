f428b79e57494f586127e9b47492f91c16338657
nary <narylozano@gmail.com>
2023-05-29 22:15:08 +0200

    Add Loginpage test and update login page image with aria-label

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -6,6 +6,7 @@ const LoginPage = (): React.ReactElement => {
       <img
         src="./media/Logotype.svg"
         alt="The Shade of it all with drawing of Sasha Velour"
+        aria-label="Logotype of The Shade of it All"
       />
     </LoginPageStyled>
   );
