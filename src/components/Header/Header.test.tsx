a9fe82603dfa85d543d909dfe1945e18cd59e3e4
nary <narylozano@gmail.com>
2023-05-30 16:40:13 +0200

    Refactor Navitagion test to get by accesible names

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,33 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import Header from "./Header";
-import { PropsWithChildren } from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { store } from "../../store";
-import GlobalStyle from "../../styles/Globalstyle";
-import theme from "../../styles/theme/theme";
+import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 
-const WrapperWithProviders = ({
-  children,
-}: PropsWithChildren): React.ReactElement => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Provider store={store}>{children}</Provider>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-};
 describe("Given a Header component", () => {
   describe("When it's rendered", () => {
     test("Then in should show a logotype of The shade of it all with its alternative text", () => {
       const altTextLogotype =
         "The Shade of it all with drawing of Sasha Velour";
 
-      render(<Header />, { wrapper: WrapperWithProviders });
-
+      renderWithProviders(wrapWithRouter(<Header />));
       const logotype = screen.getByAltText(altTextLogotype);
 
       expect(logotype).toBeInTheDocument();
--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -5,36 +5,34 @@ import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 
 describe("Given a Navigation component", () => {
   describe("When its rendered", () => {
-    test("Then it should show 3 navigation links, 'ADD'", () => {
-      const expectedTextLinkAdd = "Add";
-
-      const expectedTextLinkLogout = "Logout";
+    test("Then it should show a navigation link with  'ADD'", () => {
+      const expectedTextLinkAdd = "to Add page";
 
       renderWithProviders(wrapWithRouter(<Navigation />));
 
-      const linkAdd = screen.getByText(expectedTextLinkAdd);
-
-      const linkLogout = screen.getByText(expectedTextLinkLogout);
+      const linkAdd = screen.getByRole("link", { name: expectedTextLinkAdd });
 
       expect(linkAdd).toBeInTheDocument();
-
-      expect(linkLogout).toBeInTheDocument();
     });
+
     test("Then it should show a navigation link with 'HOME'", () => {
-      const expectedTextLinkHome = "Home";
+      const expectedTextLinkHome = "to Home page";
 
       renderWithProviders(wrapWithRouter(<Navigation />));
 
-      const linkHome = screen.getByText(expectedTextLinkHome);
+      const linkHome = screen.getByRole("link", { name: expectedTextLinkHome });
 
       expect(linkHome).toBeInTheDocument();
     });
+
     test("Then it should show a navigation Link with 'LOGOUT'", () => {
-      const expectedTextLinkLogout = "Logout";
+      const expectedTextLinkLogout = "logout of app";
 
       renderWithProviders(wrapWithRouter(<Navigation />));
 
-      const linkLogout = screen.getByText(expectedTextLinkLogout);
+      const linkLogout = screen.getByRole("link", {
+        name: expectedTextLinkLogout,
+      });
 
       expect(linkLogout).toBeInTheDocument();
     });
