feb96081b5d12a71549ecaad5930b9137b84d99e
nary <narylozano@gmail.com>
2023-05-30 20:14:03 +0200

    Add paths variables and update layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,14 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { Suspense } from "react";
 import ContainertStyled from "../shared/ContainerStyled";
-import { LazyHeader } from "../../routers/lazyComponents/lazyComponents";
+import Header from "../Header/Header";
+import paths from "../../routers/paths/paths";
 
 const Layout = (): React.ReactElement => {
   const location = useLocation();
 
   return (
     <ContainertStyled>
-      {location.pathname !== "/login" && (
-        <Suspense>
-          <LazyHeader />
-        </Suspense>
-      )}
+      {location.pathname === paths.login || <Header />}
       <Outlet />
     </ContainertStyled>
   );
