a63fa357fef7fa5830b5ae8a3d06fd3947b58fad
nary <narylozano@gmail.com>
2023-05-31 13:31:56 +0200

    Add onSubmit function

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,16 @@
 import Login from "../../components/Login/Login";
+import useUser from "../../hooks/useUser";
+import { UserCredentials } from "../../store/user/types";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = (): React.ReactElement => {
+  const { getUserToken } = useUser();
+
+  const onSubmit = async (user: UserCredentials) => {
+    const token = await getUserToken(user);
+    localStorage.setItem("token", token);
+  };
+
   return (
     <LoginPageStyled>
       <img
@@ -10,7 +19,7 @@ const LoginPage = (): React.ReactElement => {
         alt="The Shade of it all with drawing of Sasha Velour"
         aria-label="Logotype of The Shade of it All"
       />
-      <Login loginSubmit={() => ({})} />
+      <Login loginSubmit={onSubmit} />
     </LoginPageStyled>
   );
 };
