7fc294d6b74e2a19e6e9b608ca80d8fab141a06f
nary <narylozano@gmail.com>
2023-06-01 22:02:44 +0200

    Implementation of logout at navigation bar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import NavigationStyled from "./NavigationStyled";
+import { useAppDispatch } from "../../store";
+import { logoutUserActionCreator } from "../../store/user/userSlice";
+import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
+import paths from "../../routers/paths/paths";
 
 const Navigation = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+  const { deleteItemLocalStorage } = useLocalStorage();
+  const Navigate = useNavigate();
+
+  const handlingLogout = () => {
+    deleteItemLocalStorage("token");
+    dispatch(logoutUserActionCreator());
+
+    Navigate(paths.root, { replace: true });
+  };
+
   return (
     <NavigationStyled>
       <NavLink to={"/"}>Add</NavLink>
-      <NavLink to={"/"}>Home</NavLink>
-      <button onClick={() => ({})} className="logout">
+      <NavLink to={"/home"}>Home</NavLink>
+      <button onClick={handlingLogout} className="logout">
         Logout
       </button>
     </NavigationStyled>
