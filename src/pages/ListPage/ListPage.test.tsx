da37278d05add6096967cd01dd5d758dc2a2d517
nary <narylozano@gmail.com>
2023-06-02 11:22:55 +0200

    Listpage Page with heading test

---
+++ src/pages/ListPage/ListPage.test.tsx
@@ -0,0 +1,17 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import ListPage from "./ListPage";
+
+describe("Given a ListPAge page", () => {
+  describe("When it's rendered", () => {
+    test("Then it should show a heading ", () => {
+      const expectedTitle = "Drag Queens";
+
+      renderWithProviders(wrapWithRouter(<ListPage />));
+
+      const title = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/pages/ListPage/ListPageStyled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ListPageStyled = styled.main`
+  padding-left: 1.25rem;
+  color: #b24195e6;
+`;
+export default ListPageStyled;
