39f2334d56136eea2b7f8dc9fba15fc2526af03a
nary <narylozano@gmail.com>
2023-06-04 15:29:03 +0200

    Add test hideLoading to uiReducr

--- src/store/ui/uiReducer.test.ts
+++ src/store/ui/uiReducer.test.ts
@@ -1,17 +1,34 @@
 import {
   UiStateStruture,
+  hideLoadingActionCreator,
   showLoadingActionCreator,
   uiReducer,
 } from "./uiSlice";
 
 describe("Given an uiReducer", () => {
-  describe("When it receives an ui state with a is loading false", () => {
-    test("Then it should return the new state of the user", () => {
+  describe("When it receives an ui state with a isLoading false", () => {
+    test("Then it should return the new state of the ui", () => {
       const initialUiState: UiStateStruture = { isLoading: false };
       const newUiState: UiStateStruture = { isLoading: true };
 
-      const neUiState = uiReducer(initialUiState, showLoadingActionCreator());
-      expect(neUiState).toStrictEqual(newUiState);
+      const resultUiState = uiReducer(
+        initialUiState,
+        showLoadingActionCreator()
+      );
+      expect(resultUiState).toStrictEqual(newUiState);
+    });
+  });
+  describe("When it receives a ui state with a isLoading true", () => {
+    test("Then it should return the new state of the Ui", () => {
+      const initialUiState: UiStateStruture = { isLoading: true };
+      const newUiState: UiStateStruture = { isLoading: false };
+
+      const resultUiState = uiReducer(
+        initialUiState,
+        hideLoadingActionCreator()
+      );
+
+      expect(resultUiState).toStrictEqual(newUiState);
     });
   });
 });
