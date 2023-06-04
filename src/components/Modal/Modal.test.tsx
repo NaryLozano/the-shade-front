5029828ed897a5be5e2e5c81cf6682eb49a34715
nary <narylozano@gmail.com>
2023-06-04 22:41:36 +0200

    Add Modal test

---
+++ src/components/Modal/Modal.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import Modal from "./Modal";
+import modalData from "./modalData";
+
+describe("Given a Modal component", () => {
+  describe("When it's rendered with an error message", () => {
+    test("Then it should show the title of error 'OOPS!'", () => {
+      const expectedTitle = "oops!";
+
+      renderWithProviders(
+        wrapWithRouter(
+          <Modal
+            className={modalData.className.error}
+            modaltype={modalData.modaltype.error}
+            text={modalData.messages.failed}
+          />
+        )
+      );
+      screen.debug();
+      const errorTitle = screen.getByRole("heading", {
+        level: 1,
+        name: "oops!",
+      });
+
+      expect(errorTitle).toHaveAccessibleName(expectedTitle);
+    });
+  });
+});
