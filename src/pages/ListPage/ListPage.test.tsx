420516dfbee1e606fccb6fd7b0a85b1c8b35fec1
nary <narylozano@gmail.com>
2023-06-05 23:24:03 +0200

    Add modal test at list page

--- src/pages/ListPage/ListPage.test.tsx
+++ src/pages/ListPage/ListPage.test.tsx
@@ -1,8 +1,11 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import ListPage from "./ListPage";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "../../styles/theme/theme";
 
-describe("Given a ListPAge page", () => {
+describe("Given a ListPage page", () => {
   describe("When it's rendered", () => {
     test("Then it should show a heading ", () => {
       const expectedTitle = "Drag Queens";
@@ -12,6 +15,33 @@ describe("Given a ListPAge page", () => {
       const title = screen.getByRole("heading", { name: expectedTitle });
 
       expect(title).toBeInTheDocument();
+    });
+  });
+  describe("When its rendered and cant get the queens", () => {
+    test("Then it should Show a feedback error", async () => {
+      const expectedText = "oops!";
+      const routes = [
+        {
+          path: "/",
+          element: (
+            <ThemeProvider theme={theme}>
+              <ListPage />
+            </ThemeProvider>
+          ),
+        },
+      ];
+
+      const routerList = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={routerList} />, {
+        ui: { isError: true },
+      });
+
+      const modalError = await waitFor(() =>
+        screen.getByRole("heading", { level: 2, name: expectedText })
+      );
+
+      expect(modalError).toBeInTheDocument();
     });
   });
 });
--- src/pages/ListPage/ListPageStyled.ts
+++ src/pages/ListPage/ListPageStyled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const ListPageStyled = styled.main`
   padding-left: 1.25rem;
-  color: #b24195e6;
+  color: ${(props) => props.theme.colors.darkSecondary};
   display: flex;
   flex-direction: column;
   align-items: center;
