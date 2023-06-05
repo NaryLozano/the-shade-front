0261bb0197b121f04c8566259a277b91c86354f3
nary <narylozano@gmail.com>
2023-06-05 23:10:56 +0200

    Update styles of layout and matcher at test layout

---
+++ src/components/Layout/LayoutStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.563rem;
+  margin: 0 auto;
+  min-width: 20rem;
+  min-height: 100vh;
+  position: relative;
+`;
+
+export default LayoutStyled;
