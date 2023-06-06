6a30a664d19b468ca22fe2194c6e26474210c94f
nary <narylozano@gmail.com>
2023-06-06 18:31:13 +0200

    Refactor modal test

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,27 +1,32 @@
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
-import Modal from "./Modal";
-import modalData from "../../data/modalData";
+import { renderWithProviders } from "../../utils/testUtils";
 import { ThemeProvider } from "styled-components";
 import theme from "../../styles/theme/theme";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import ListPage from "../../pages/ListPage/ListPage";
+import Layout from "../Layout/Layout";
 
 describe("Given a Modal component", () => {
+  const routes = [
+    {
+      path: "/",
+      element: (
+        <ThemeProvider theme={theme}>
+          <Layout />
+        </ThemeProvider>
+      ),
+    },
+  ];
+
+  const modal = createMemoryRouter(routes);
+
   describe("When it's rendered with an error message", () => {
     test("Then it should show the title of error 'OOPS!'", () => {
       const expectedTitle = "oops!";
 
-      renderWithProviders(
-        wrapWithRouter(
-          <Modal
-            className={modalData.className.error}
-            modaltype={modalData.modaltype.error}
-            text={modalData.messages.failed}
-          />
-        )
-      );
+      renderWithProviders(<RouterProvider router={modal} />, {
+        ui: { isSuccess: false, feedback: true },
+      });
 
       const errorTitle = screen.getByRole("heading", {
         level: 2,
@@ -34,20 +39,8 @@ describe("Given a Modal component", () => {
 
   describe("When its rendered and receives a user click on the button", () => {
     test("Then it shouldn't show the modal", async () => {
-      const routes = [
-        {
-          path: "/",
-          element: (
-            <ThemeProvider theme={theme}>
-              <ListPage />
-            </ThemeProvider>
-          ),
-        },
-      ];
-
-      const modal = createMemoryRouter(routes);
       renderWithProviders(<RouterProvider router={modal} />, {
-        ui: { isSuccess: true },
+        ui: { isSuccess: false, feedback: true },
       });
 
       const backButton = screen.getByRole("button", { name: "back" });
